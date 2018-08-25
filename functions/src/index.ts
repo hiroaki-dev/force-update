4770844685b7b544c3e125380a12fa27d979e230
Yuki Nakakura <hiroaki.dev@gmail.com>
2018-08-26 02:45:03 +0900

    lint対応

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,22 @@
-const admin = require('firebase-admin');
-const functions = require('firebase-functions');
+import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
+
 
 admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
 
+export class Response {
+    isUpdate: boolean;
+    nowVersion: number;
+    constructor(have2Update: boolean, argVersion: number) {
+        this.isUpdate = have2Update;
+        this.nowVersion = argVersion;
+    }
+}
+
 export const isUpdate = functions.https.onRequest((req, res) => {
     const appId = req.query.appId;
-    const nowVersion = req.query.nowVersion;
+    const nowVersion = parseInt(req.query.nowVersion);
 
     const app = db.collection('apps').doc(appId);
     app.get().then(snapshot => {
@@ -17,19 +27,10 @@ export const isUpdate = functions.https.onRequest((req, res) => {
 
         data.updateVersions.forEach(element => {
             // console.log("element = " + element + ". isSame? " + element == nowVersion);
-            if (element == nowVersion) have2Update = true;
+            if (element === nowVersion) have2Update = true;
         });
         res.status(200).json(new Response(have2Update, nowVersion)).end();
     }).catch(error => {
         res.status(500).end();
     });
 })
-
-class Response {
-    isUpdate: boolean;
-    nowVersion: number;
-    constructor(isUpdate: boolean, nowVersion: number) {
-        this.isUpdate = isUpdate;
-        this.nowVersion = nowVersion
-    }
-}
